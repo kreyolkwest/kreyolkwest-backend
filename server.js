e55fa74e6601bbc3cbca80ecc33cd01b5af936ec
xavierbeaunol <xavier.beaunol001@gmail.com>
2025-04-01 15:46:15 +0200

    admin parametrage

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import cors from 'cors'
+
 
 // Import des routes
 import  activityRoutes from './routes/activity.routes.js';
