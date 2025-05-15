226045605fa06ff302251a9ec5fdb03bb5610f79
xavierbeaunol <xavier.beaunol001@gmail.com>
2025-05-15 21:24:02 +0200

    chang

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ const allowedOrigins = [
   'https://kreyolkwest-frontend.onrender.com',
   'http://localhost:5173', 
   'http://localhost:5004',
-  'http://localhost:5013'
+  'http://localhost:5013',
+  'https://www.kreyolkwest.com/home'
 ]
 
 const corsOptions = {
